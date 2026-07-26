93df65bd425af7cba7fba9aa32143d13dc1adc2e
YSH <10471270+zero41120@users.noreply.github.com>
2026-07-25 21:44:06 -0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+    <h1>It works</h1>
+    <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </>
+  );
+}
+
+
+export default App;
